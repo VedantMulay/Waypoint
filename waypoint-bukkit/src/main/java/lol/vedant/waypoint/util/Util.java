c1110b41e1edee0f243025eb7e1a28f6493fadaa
VedantMulay <comphackgamer@gmail.com>
2026-05-03 17:04:10 +0530

    Add coordinate roundup

--- waypoint-bukkit/src/main/java/lol/vedant/waypoint/util/Util.java
+++ waypoint-bukkit/src/main/java/lol/vedant/waypoint/util/Util.java
@@ -12,11 +12,11 @@ public class Util {
 
     public static Location fromString(String loc) {
         String[] locs = loc.split(", ");
-        return new Location(Bukkit.getWorld(locs[0]), Double.parseDouble(locs[1]), Double.parseDouble(locs[2]), Double.parseDouble(locs[3]));
+        return new Location(Bukkit.getWorld(locs[0]), Integer.parseInt(locs[1]), Integer.parseInt(locs[2]), Integer.parseInt(locs[3]));
     }
 
     public static String fromLocation(Location loc) {
-        return loc.getWorld().getName() + ", " + loc.getX() + ", " + loc.getY() + ", " + loc.getZ();
+        return loc.getWorld().getName() + ", " + loc.getBlockX() + ", " + loc.getBlockY() + ", " + loc.getBlockZ();
     }
 
     public static String cc(String text) {
